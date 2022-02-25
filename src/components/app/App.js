a7ee2ecddcf06efcbcf94df42a4e561e50f8a820
Tzipora Gutmann <rgutmann4@student.touro.edu>
2022-02-25 11:11:13 -0500

    added a header component and a router to create menu options to navigate to different pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import React from "react";
 import { Home } from "../home/home";
 import { ToDo } from "../todo/todo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "../header/header";
 
 function App() {
   return (
     <div>
-      <Home />
-      <ToDo />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" element={<ToDo />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
