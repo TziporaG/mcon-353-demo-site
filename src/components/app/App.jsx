f0f62252fc6a4de2db724774fd988a11e3df6431
Tzipora Gutmann <rgutmann4@student.touro.edu>
2022-03-28 12:19:41 -0400

    cleaned up commented out code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,38 +6,8 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { Header } from "../header/header";
 import { Chat } from "../chat/chat";
 import { TodoProvider } from "./context";
-/*
-var initialList = [];
-const changeToDoListReducer = (state, action) => {
-  var newTodos = [];
-  switch (action.type) {
-    case "addItem":
-      newTodos = [...state, { text: action.text, isCompleted: false }];
-      return newTodos;
 
-    case "removeItem":
-      newTodos = [...state];
-      newTodos.splice(action.index, 1);
-      return newTodos;
-
-    case "completeItem":
-      newTodos = [...state];
-      newTodos[action.index].isCompleted = true;
-      return newTodos;
-
-    default:
-      throw new Error(`Count Reducer does not recognize ${action.type}`);
-  }
-};
-
-export const ListContext = React.createContext();
-*/
 function App() {
-  /*const [toDoState, dispatchChangeToDoList] = useReducer(
-    changeToDoListReducer,
-    initialList
-  );*/
-
   return (
     <TodoProvider>
       <HashRouter>
